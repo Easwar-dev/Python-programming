fc9aecbf3408e2d69f0540c04c72aaf13498685e
Easwar <host57203@gmail.com>
2025-10-30 05:07:25 +0530

    Arrays in python.

---
+++ arrays.py
@@ -0,0 +1,18 @@
+# Arrays (called lists in python)
+arr = [1, 2, 3]
+print(arr)
+
+# Can be used as a stack
+arr.append(4)
+arr.append(5)
+print(arr)
+
+arr.pop()
+print(arr)
+
+arr.insert(1, 7)
+print(arr)
+
+arr[0] = 0
+arr[3] = 0
+print(arr)
