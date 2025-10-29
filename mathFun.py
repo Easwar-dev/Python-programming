d57d7f2029bd9d8dc00edd5531002544ee3c4c0c
Easwar <host57203@gmail.com>
2025-10-30 05:00:52 +0530

    math function in python.

---
+++ mathFun.py
@@ -0,0 +1,20 @@
+# To be consistent with other language module
+import math
+from multiprocessing import heap
+print(math.fmod(-10, 3))
+
+# More math helpers
+print(math.floor(3 / 2))
+print(math.ceil(3 / 2))
+print(math.sqrt(2))
+print(math.pow(2, 3))
+
+# Max / Min Int
+float("inf")
+float("-inf")
+
+# Python numbers are infinite so they never overflow
+print(math.pow(2, 200))
+
+# But still less than infinity
+print(math.pow(2, 200) < float("inf"))
