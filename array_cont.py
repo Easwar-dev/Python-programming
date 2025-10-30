9e3768600f78949386dc2ef93035338d1d6c5221
Easwar <host57203@gmail.com>
2025-10-31 03:54:50 +0530

    Arrays (cont) in python updated.

--- array_cont.py
+++ array_cont.py
@@ -24,3 +24,57 @@ print(a, b, c)
 
 # Be careful though
 # a, b = [1, 2, 3]
+
+# Loop through arrays
+nums = [1, 2, 3]
+
+# Using index
+for i in range(len(nums)):
+    print(nums[i])
+
+# Without index
+for n in nums:
+    print(n)
+
+# With index and value
+for i, n in enumerate(nums):
+    print(i, n)
+
+# Loop through multiple arrays simultaneously with unpacking
+nums1 = [1, 3, 5]
+nums2 = [2, 4, 6]
+for n1, n2 in zip(nums1, nums2):
+    print(n1, n2)
+
+# Reverse
+nums = [1, 2, 3]
+nums.reverse()
+print(nums)
+
+# Sorting
+arr = [5, 4, 7, 3, 8]
+arr.sort()
+print(arr)
+
+arr.sort(reverse=True)
+print(arr)
+
+arr = ["bob", "alice", "jane", "doe"]
+arr.sort()
+print(arr)
+
+#Custom sort (by length of string)
+arr.sort(key=lambda x: len(x))
+print(arr)
+
+# List comprehension
+arr = [i for i in range(4)]
+print(arr)
+
+# 2-D lists
+arr = [[0] * 4 for i in range(4)]
+print(arr)
+print(arr[0][0], arr[3][3])
+
+# This won't work
+# arr = [[0] * 4] * 4
