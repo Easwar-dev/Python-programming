01fc8d3a7359d3e9fb39d63858b0dd110d73a128
Easwar <host57203@gmail.com>
2025-12-17 20:23:27 +0530

    password manager using cryptography module

---
+++ Projects/pass_man.py
@@ -0,0 +1,54 @@
+from cryptography.fernet import Fernet
+
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key',"wb") as key_file:
+        key_file.write(key)
+write_key()
+
+def load_key():
+    file = open("key.key","rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+fer = Fernet(key)
+
+
+def view():
+    with open('password.txt', 'r') as f:
+        for line in f:
+            data = line.rstrip()
+            if "|" not in data:
+                continue
+            user, passw = data.split("|")
+            try:
+                print(
+                    "User:", user,
+                    ", Password:",
+                    fer.decrypt(passw.encode()).decode()
+                )
+            except Exception:
+                print("Error decrypting password for", user)
+
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open('password.txt','a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+
+while True:
+    mode = input("would you like to add a new password or view existing ones (view, add),press q to quit? ")
+    if mode == 'q':
+        break
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
