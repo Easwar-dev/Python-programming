34ad15c74652add226a017c0e7ec37a84c092b5f
Easwar <host57203@gmail.com>
2025-12-24 21:43:32 +0530

    math challenge project completed

--- Projects/math_challenge.py
+++ Projects/math_challenge.py
@@ -1,8 +1,10 @@
 import random
+import time
 
 operators = ["+","-","*"]
 min_operand = 3
 max_operand = 12
+total_problems = 10
 
 def generate_problem():
     left = random.randint(min_operand, max_operand)
@@ -10,4 +12,24 @@ def generate_problem():
     operator = random.choice(operators)
 
     expr = str(left) + " " + operator + " " + str(right)
-    print(expr)
+    answer = eval(expr)
+    return expr, answer
+
+wrong = 0
+input("press enter to start!")
+print("----------------------")
+
+start_time = time.time()
+
+for i in range(total_problems):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("problem #" + str(i + 1) + ": " + expr + " = ")
+        if guess == str(answer):
+            break
+        wrong += 1
+    end_time = time.time()
+    total_time = round(end_time - start_time)
+
+print("----------------------")
+print("Nice work! you finish in", total_time, "seconds!")
