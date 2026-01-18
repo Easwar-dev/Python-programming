f9a71c98dee756da8028bab36808d4760abbe05d
Easwar <host57203@gmail.com>
2026-01-18 23:03:36 +0530

    mastermind game completed

--- Projects/mastermind_game.py
+++ Projects/mastermind_game.py
@@ -32,3 +32,44 @@ def guess_code():
     return guess
 
 def check_code(guess, real_code):
+    color_count = {}
+    correct_pos = 0
+    incorrect_pos = 0
+
+    for color in real_code:
+        if color not in color_count:
+            color_count[color] = 0
+        color_count[color] += 1
+
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color == real_color:
+            correct_pos += 1
+            color_count[guess_color] -= 1
+
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color in color_count and color_count[guess_color] > 0:
+            incorrect_pos += 1
+            color_count[guess_color] -= 1
+
+    return correct_pos, incorrect_pos
+
+def game():
+    print(f"Welcome to mastermind, you have {TRIES} to guess the code...")
+    print("The valid colors are", *COLORS)
+
+    code = generate_code()
+    for attempts in range(1, TRIES + 1):
+        guess = guess_code()
+        correct_pos, incorrect_pos = check_code(guess, code)
+
+        if correct_pos == CODE_LENGTH:
+            print(f"You guessed the code in {attempts} tries!")
+            break
+
+        print(f"Correct Positions: {correct_pos} | Incorrect Positions: {incorrect_pos}")
+
+    else:
+        print("You ran out of tries, the code was:", *code)
+
+if __name__ == "__main__":
+    game()
