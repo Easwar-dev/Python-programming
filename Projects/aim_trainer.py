50d015f35c3fc5221c5a68d1105f3920b4e0fa8c
Easwar <host57203@gmail.com>
2026-01-25 23:29:45 +0530

    event padding is added.

--- Projects/aim_trainer.py
+++ Projects/aim_trainer.py
@@ -10,6 +10,11 @@ WIDTH, HEIGHT = 800,600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aim Trainer")
 
+TARGET_INCREMENT = 400
+TARGET_EVENT = pygame.USEREVENT
+
+TARGET_PADDING = 30
+
 class Target:
     MAX_SIZE = 30
     GROWTH_RATE = 0.2
@@ -39,12 +44,21 @@ class Target:
 
 def main():
     run = True
+    targets = []
+
+    pygame.time.set_timer(TARGET_EVENT, TARGET_INCREMENT)
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+            if event.type == TARGET_EVENT:
+                x = random.randint(TARGET_PADDING, WIDTH - TARGET_PADDING)
+                y = random.randint(TARGET_PADDING, HEIGHT - TARGET_PADDING)
+                target = Target(x, y)
+                targets.append(target)
 
     pygame.quit()
 
